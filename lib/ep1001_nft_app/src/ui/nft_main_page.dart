f3cc8e8d25eb1c68493ce90a014c3e28084462d0
Dreamwalker <aristojeff@gmail.com>
2021-12-13 19:40:32 +0900

    Update nft_main_page.dart

--- lib/ep1001_nft_app/src/ui/nft_main_page.dart
+++ lib/ep1001_nft_app/src/ui/nft_main_page.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-class NftMainPage extends StatelessWidget {
+import 'package:flutter_notebook_17th/ep1001_nft_app/src/provider/nft_btm_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class NftMainPage extends ConsumerWidget {
   const NftMainPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[50],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        currentIndex: ,
+        currentIndex: ref.read(nftBottomProvider),
+        onTap: (idx) {
+          ref.read(nftBottomProvider.notifier).state = idx;
+        },
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
@@ -19,4 +25,3 @@ class NftMainPage extends StatelessWidget {
     );
   }
 }
-
