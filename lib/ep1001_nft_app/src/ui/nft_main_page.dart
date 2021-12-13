8245db1429dc89f2254099383374cea15bfb9150
Dreamwalker <aristojeff@gmail.com>
2021-12-13 19:33:05 +0900

    Create nft_main_page.dart

---
+++ lib/ep1001_nft_app/src/ui/nft_main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class NftMainPage extends StatefulWidget {
+  const NftMainPage({Key? key}) : super(key: key);
+
+  @override
+  _NftMainPageState createState() => _NftMainPageState();
+}
+
+class _NftMainPageState extends State<NftMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[50],
+    );
+  }
+}
