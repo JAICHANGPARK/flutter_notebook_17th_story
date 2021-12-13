086cfa6ba4aa960b53e58d4c8d2efb875e594264
Dreamwalker <aristojeff@gmail.com>
2021-12-13 19:28:47 +0900

    Create nft_app.dart

---
+++ lib/ep1001_nft_app/nft_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class NtfApp extends StatelessWidget {
+  const NtfApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
