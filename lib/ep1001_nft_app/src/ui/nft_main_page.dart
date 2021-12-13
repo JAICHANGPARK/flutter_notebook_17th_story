a310238e292630983350c27ffb78530d162a51b6
Dreamwalker <aristojeff@gmail.com>
2021-12-13 19:39:09 +0900

    Update nft_main_page.dart

--- lib/ep1001_nft_app/src/ui/nft_main_page.dart
+++ lib/ep1001_nft_app/src/ui/nft_main_page.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-
-
-class NftMainPage extends StatefulWidget {
+class NftMainPage extends StatelessWidget {
   const NftMainPage({Key? key}) : super(key: key);
 
-  @override
-  _NftMainPageState createState() => _NftMainPageState();
-}
-
-class _NftMainPageState extends State<NftMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +10,13 @@ class _NftMainPageState extends State<NftMainPage> {
         type: BottomNavigationBarType.fixed,
         currentIndex: ,
         items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-      ],
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        ],
       ),
     );
   }
 }
+
