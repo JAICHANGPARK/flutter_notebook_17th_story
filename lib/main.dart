fc17edbc6fc5e1158bd792b01226d17510aadbb6
Dreamwalker <aristojeff@gmail.com>
2021-12-13 19:44:15 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_17th/ep1001_nft_app/nft_app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 void main() {
