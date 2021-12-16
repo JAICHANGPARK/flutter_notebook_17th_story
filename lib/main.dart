59f9c32496f87209a9fd31f0539b4b774e5add2c
Dreamwalker <aristojeff@gmail.com>
2021-12-16 19:40:34 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1001_nft_app/nft_app.dart';
+import 'package:flutter_notebook_17th/ep1004_finance_app/finacne_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 void main() {
-  runApp(ProviderScope(child: NftApp()));
+  runApp(ProviderScope(child: FinanceApp()));
 }
