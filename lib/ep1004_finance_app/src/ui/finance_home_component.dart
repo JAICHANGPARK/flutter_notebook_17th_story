40a5d86219b8d48a5b6793faab1863a26156d4db
Dreamwalker <aristojeff@gmail.com>
2021-12-17 16:30:32 +0900

    Create finance_home_component.dart

---
+++ lib/ep1004_finance_app/src/ui/finance_home_component.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class FinanceHomeComponent extends StatelessWidget {
+  const FinanceHomeComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
