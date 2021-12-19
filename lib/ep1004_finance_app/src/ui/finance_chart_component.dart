b1a2a94911ffc7966a253b0ed2aee4f4bae63557
Dreamwalker <aristojeff@gmail.com>
2021-12-19 17:01:42 +0900

    Update finance_chart_component.dart

--- lib/ep1004_finance_app/src/ui/finance_chart_component.dart
+++ lib/ep1004_finance_app/src/ui/finance_chart_component.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FinanceChartComponent extends StatefulWidget {
   const FinanceChartComponent({Key? key}) : super(key: key);
 
@@ -13,8 +12,18 @@ class _FinanceChartComponentState extends State<FinanceChartComponent> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Analysis",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+            ),
+          ),
         ],
       ),
     );
