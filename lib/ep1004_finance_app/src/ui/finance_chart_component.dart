840b6c11f5dea89410450785d276f3aa46ebf9f7
Dreamwalker <aristojeff@gmail.com>
2021-12-19 16:59:59 +0900

    Update finance_chart_component.dart

---
+++ lib/ep1004_finance_app/src/ui/finance_chart_component.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class FinanceChartComponent extends StatefulWidget {
+  const FinanceChartComponent({Key? key}) : super(key: key);
+
+  @override
+  _FinanceChartComponentState createState() => _FinanceChartComponentState();
+}
+
+class _FinanceChartComponentState extends State<FinanceChartComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
