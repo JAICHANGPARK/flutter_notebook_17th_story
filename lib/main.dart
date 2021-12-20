15f60fa2647931e906ff84dd47357ee273bf7a9a
Dreamwalker <aristojeff@gmail.com>
2021-12-20 16:55:27 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1009_badylon_app/babylon_app.dart';
+import 'package:flutter_notebook_17th/ep1011_vehicle_retailer_app/vehicle_retailer_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 void main() {
-  runApp(ProviderScope(child: BabylonApp()));
+  runApp(ProviderScope(child: VehicleRetailerApp()));
 }
