c29a734ec569ffff57fb2750184036c2e06a705d
Dreamwalker <aristojeff@gmail.com>
2021-12-20 15:29:07 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1004_finance_app/finacne_app.dart';
+import 'package:flutter_notebook_17th/ep1009_badylon_app/babylon_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 void main() {
-  runApp(ProviderScope(child: FinanceApp()));
+  runApp(ProviderScope(child: BabylonApp()));
 }
