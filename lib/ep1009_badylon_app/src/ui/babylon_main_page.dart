42f24edbc0a54293109931466f8825d67b7ebe55
Dreamwalker <aristojeff@gmail.com>
2021-12-20 15:27:46 +0900

    Create babylon_main_page.dart

---
+++ lib/ep1009_badylon_app/src/ui/babylon_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BabylonMainPage extends StatefulWidget {
+  const BabylonMainPage({Key? key}) : super(key: key);
+
+  @override
+  _BabylonMainPageState createState() => _BabylonMainPageState();
+}
+
+class _BabylonMainPageState extends State<BabylonMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
