495ae39dce8f7cbe2f0434200af672fdf25e2f2c
Dreamwalker <aristojeff@gmail.com>
2021-12-20 16:53:54 +0900

    Create vehicle_retailer_app.dart

---
+++ lib/ep1011_vehicle_retailer_app/vehicle_retailer_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class VehicleRetailerApp extends StatelessWidget {
+  const VehicleRetailerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
