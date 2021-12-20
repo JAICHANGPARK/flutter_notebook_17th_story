94716ae23292da7c4daa75e2b22f19c0791a96c5
Dreamwalker <aristojeff@gmail.com>
2021-12-20 16:55:33 +0900

    Create vehicle_main_page.dart

---
+++ lib/ep1011_vehicle_retailer_app/src/vehicle_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class VehicleMainPage extends StatefulWidget {
+  const VehicleMainPage({Key? key}) : super(key: key);
+
+  @override
+  _VehicleMainPageState createState() => _VehicleMainPageState();
+}
+
+class _VehicleMainPageState extends State<VehicleMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
