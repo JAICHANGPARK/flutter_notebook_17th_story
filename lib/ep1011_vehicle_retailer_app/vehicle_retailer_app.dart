115b4fde641e07ed0c86285af07c83e218ea755d
Dreamwalker <aristojeff@gmail.com>
2021-12-24 21:40:29 +0900

    Update vehicle_retailer_app.dart

--- lib/ep1011_vehicle_retailer_app/vehicle_retailer_app.dart
+++ lib/ep1011_vehicle_retailer_app/vehicle_retailer_app.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_17th/ep1011_vehicle_retailer_app/src/vehicle_main_page.dart';
 
-
 class VehicleRetailerApp extends StatelessWidget {
   const VehicleRetailerApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: VehicleMainPage(),
+    return const MaterialApp(
+      home: VehicleMainPage(),
     );
   }
 }
