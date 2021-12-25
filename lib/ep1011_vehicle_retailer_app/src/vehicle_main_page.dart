3cff2e40ee026662cf32c8cb017f8412c3766a21
Dreamwalker <aristojeff@gmail.com>
2021-12-25 11:39:50 +0900

    Update vehicle_main_page.dart

--- lib/ep1011_vehicle_retailer_app/src/vehicle_main_page.dart
+++ lib/ep1011_vehicle_retailer_app/src/vehicle_main_page.dart
@@ -26,111 +26,126 @@ class _VehicleMainPageState extends State<VehicleMainPage> {
                 children: [
                   Expanded(
                       child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(28),
-                        ),
-                        padding: const EdgeInsets.all(16),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 24),
-                              child: Row(
-                                children: [
-                                  const Text(
-                                    "Explore",
-                                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 28),
-                                  ),
-                                  const Spacer(),
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(Icons.search),
-                                    color: Colors.white,
-                                  )
-                                ],
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(28),
+                    ),
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 24),
+                          child: Row(
+                            children: [
+                              const Text(
+                                "Explore",
+                                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 28),
                               ),
-                            ),
-                            Expanded(
-                              flex: 5,
-                                child: ListView.builder(
-                                    scrollDirection: Axis.horizontal,
-                                    itemBuilder: (context, index) {
-                                      return Padding(
-                                        padding: const EdgeInsets.only(bottom: 8, top: 8,
-                                        right: 16),
-                                        child: Container(
-                                          width: 120,
-                                          decoration: BoxDecoration(
-                                            color: Colors.blue,
-                                            borderRadius: BorderRadius.circular(16),
-                                          ),
-                                          padding: const EdgeInsets.all(8),
-                                          child: Column(
-                                            children: const [
-                                              Spacer(),
-                                              Text(
-                                                "World news of the week",
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      );
-                                    })),
-                            Expanded(
-                              flex: 4,
-                              child: Column(
-                                children: [
-                                  const Text(
-                                    'Last seen',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  Container(
-                                    height: 64,
-                                    decoration: BoxDecoration(
-                                      color: Colors.white.withOpacity(0.2),
-                                    ),
-                                    child: Row(
-                                      children: [
-                                        Container(
-                                          height: 42,
-                                          width: 42,
-                                          decoration: const BoxDecoration(
-                                            color: Colors.green,
-                                          ),
-                                        ),
-                                        Column(
-                                          children: const [Text("BMW X1 II(F48) 2019"), Text("\$35,400")],
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-
-                          ],
+                              const Spacer(),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.search),
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
                         ),
-                      )),
+                        Expanded(
+                            flex: 5,
+                            child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(bottom: 8, top: 8, right: 16),
+                                    child: Container(
+                                      width: 120,
+                                      decoration: BoxDecoration(
+                                        color: Colors.blue,
+                                        borderRadius: BorderRadius.circular(16),
+                                      ),
+                                      padding: const EdgeInsets.all(8),
+                                      child: Column(
+                                        children: const [
+                                          Spacer(),
+                                          Text(
+                                            "World news of the week",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                })),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Expanded(
+                          flex: 4,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Last seen',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 12,
+                              ),
+                              Container(
+                                height: 64,
+                                decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                padding: const EdgeInsets.all(8),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: 42,
+                                      width: 42,
+                                      decoration: const BoxDecoration(
+                                        color: Colors.green,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: const [
+                                          Text("BMW X1 II(F48) 2019"),
+                                          Text("\$35,400"),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
                   Expanded(
                       child: SingleChildScrollView(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text("Recommended"),
-                            SizedBox(
-                              height: 300,
-                              child: Column(
-                                children: [],
-                              ),
-                            )
-                          ],
-                        ),
-                      )),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Recommended"),
+                        SizedBox(
+                          height: 300,
+                          child: Column(
+                            children: [],
+                          ),
+                        )
+                      ],
+                    ),
+                  )),
                 ],
               )),
           Positioned(
