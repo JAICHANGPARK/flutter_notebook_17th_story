31d76a00d4351765a5c49b459f8cc530ccd57402
Dreamwalker <aristojeff@gmail.com>
2021-12-26 18:05:46 +0900

    Update movie_finder_app.dart

---
+++ lib/ep1014_movie_finder_app/movie_finder_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class MovieFinderApp extends StatelessWidget {
+  const MovieFinderApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
