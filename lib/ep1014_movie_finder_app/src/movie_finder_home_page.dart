cc6aef835e47957bb88d0400d99b424c4f8544a0
Dreamwalker <aristojeff@gmail.com>
2021-12-26 18:10:58 +0900

    Update movie_finder_home_page.dart

--- lib/ep1014_movie_finder_app/src/movie_finder_home_page.dart
+++ lib/ep1014_movie_finder_app/src/movie_finder_home_page.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
-
 class MovieFinderHomePage extends StatelessWidget {
   const MovieFinderHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Find movies like"),
+          Container(
+            height: 64,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey[300]!,
+                ),
+                borderRadius: BorderRadius.circular(24)),
+          )
+        ],
+      ),
+    );
   }
 }
