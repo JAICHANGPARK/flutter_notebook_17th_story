d4b5653d7aa5324d32fcb91f2182f1294a3f44e2
Dreamwalker <aristojeff@gmail.com>
2021-12-27 16:59:29 +0900

    Update movie_api.dart

--- lib/ep1014_movie_finder_app/src/api/movie_api.dart
+++ lib/ep1014_movie_finder_app/src/api/movie_api.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart' as http;
 
 Future getMovies() async {
-  var response = await http.get(Uri.parse("https://yts.mx/api/v2/list_movies.xml?sort=seeds&limit=15"));
+  print("call getMovies()");
+  var response = await http.get(Uri.parse("https://yts.mx/api/v2/list_movies.json"));
   if (response.statusCode == 200) {
     print(response.body);
   } else {
