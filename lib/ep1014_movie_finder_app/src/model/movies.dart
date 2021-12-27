149de9e6a8032a94fed3de175691241b8d5a8ec0
Dreamwalker <aristojeff@gmail.com>
2021-12-27 17:07:15 +0900

    Update movies.dart

--- lib/ep1014_movie_finder_app/src/model/movies.dart
+++ lib/ep1014_movie_finder_app/src/model/movies.dart
@@ -1,5 +1,20 @@
 
-import 'free';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'movies.freezed.dart';
+part 'movies.g.dart';
+
+@freezed
+class Movies with _$Movies{
+  factory Movies({
+    int? id,
+    String? url,
+    String? title,
+    String? titleEnglish,
+    String? titleLong,
+    String? slug,
+}) = _Movies;
+}
 
 ///
 /// {
