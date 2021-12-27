4f7d0d49cf1e6d35095567879a53b64bce512313
Dreamwalker <aristojeff@gmail.com>
2021-12-27 16:41:45 +0900

    Update movie_search_provider.dart

--- lib/ep1014_movie_finder_app/src/riverpod/movie_search_provider.dart
+++ lib/ep1014_movie_finder_app/src/riverpod/movie_search_provider.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final movieSearchProvider = Provider((ref){
-
+ return TextEditingController();
 });
