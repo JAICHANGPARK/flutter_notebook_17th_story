5456d4e0e295b8e4cce8dc06eda911b0aef44009
Dreamwalker <aristojeff@gmail.com>
2021-12-29 19:52:52 +0900

    Update movies.g.dart

--- lib/ep1014_movie_finder_app/src/model/movies.g.dart
+++ lib/ep1014_movie_finder_app/src/model/movies.g.dart
@@ -10,25 +10,31 @@ _$_Movie _$$_MovieFromJson(Map<String, dynamic> json) => _$_Movie(
       id: json['id'] as int?,
       url: json['url'] as String?,
       title: json['title'] as String?,
-      titleEnglish: json['titleEnglish'] as String?,
-      titleLong: json['titleLong'] as String?,
+      titleEnglish: json['title_english'] as String?,
+      titleLong: json['title_long'] as String?,
       slug: json['slug'] as String?,
+      backgroundImage: json['background_image'] as String?,
+      smallCoverImage: json['small_cover_image'] as String?,
+      mediumCoverImage: json['medium_cover_image'] as String?,
     );
 
 Map<String, dynamic> _$$_MovieToJson(_$_Movie instance) => <String, dynamic>{
       'id': instance.id,
       'url': instance.url,
       'title': instance.title,
-      'titleEnglish': instance.titleEnglish,
-      'titleLong': instance.titleLong,
+      'title_english': instance.titleEnglish,
+      'title_long': instance.titleLong,
       'slug': instance.slug,
+      'background_image': instance.backgroundImage,
+      'small_cover_image': instance.smallCoverImage,
+      'medium_cover_image': instance.mediumCoverImage,
     };
 
 _$_MovieDatas _$$_MovieDatasFromJson(Map<String, dynamic> json) =>
     _$_MovieDatas(
-      movieCount: json['movieCount'] as int?,
+      movieCount: json['movie_count'] as int?,
       limit: json['limit'] as int?,
-      pageNumber: json['pageNumber'] as int?,
+      pageNumber: json['page_number'] as int?,
       movies: (json['movies'] as List<dynamic>?)
           ?.map((e) => Movie.fromJson(e as Map<String, dynamic>))
           .toList(),
@@ -36,22 +42,22 @@ _$_MovieDatas _$$_MovieDatasFromJson(Map<String, dynamic> json) =>
 
 Map<String, dynamic> _$$_MovieDatasToJson(_$_MovieDatas instance) =>
     <String, dynamic>{
-      'movieCount': instance.movieCount,
+      'movie_count': instance.movieCount,
       'limit': instance.limit,
-      'pageNumber': instance.pageNumber,
+      'page_number': instance.pageNumber,
       'movies': instance.movies,
     };
 
 _$_Movies _$$_MoviesFromJson(Map<String, dynamic> json) => _$_Movies(
       status: json['status'] as String?,
-      statusMessage: json['statusMessage'] as String?,
-      data: (json['data'] as List<dynamic>?)
-          ?.map((e) => MovieDatas.fromJson(e as Map<String, dynamic>))
-          .toList(),
+      statusMessage: json['status_message'] as String?,
+      data: json['data'] == null
+          ? null
+          : MovieDatas.fromJson(json['data'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$$_MoviesToJson(_$_Movies instance) => <String, dynamic>{
       'status': instance.status,
-      'statusMessage': instance.statusMessage,
+      'status_message': instance.statusMessage,
       'data': instance.data,
     };
