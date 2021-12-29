b0c568b06a741bea476c90e3a8b8404a404ce1e6
Dreamwalker <aristojeff@gmail.com>
2021-12-29 20:02:06 +0900

    Update movie_api.dart

--- lib/ep1014_movie_finder_app/src/api/movie_api.dart
+++ lib/ep1014_movie_finder_app/src/api/movie_api.dart
@@ -2,24 +2,22 @@ import 'package:flutter_notebook_17th/ep1014_movie_finder_app/src/model/movies.d
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future getMovies() async {
+Future<Movies?> getMovies() async {
   print("call getMovies()");
   var response = await http.get(Uri.parse("https://yts.mx/api/v2/list_movies.json"));
   if (response.statusCode == 200) {
-    print(response.body);
-    print(jsonDecode(response.body));
+    // print(response.body);
+    // print(jsonDecode(response.body));
     Movies movies = Movies.fromJson(json.decode(response.body));
 
-    print(movies.status);
-    print(movies.statusMessage);
-    print(movies.data?.movies?.length);
+    // print(movies.status);
+    // print(movies.statusMessage);
+    // print(movies.data?.movies?.length);
     int len = movies.data?.movies?.length ?? 0;
-    for(int i =0; i<len; i++){
-      print(movies.data?.movies?[i].backgroundImage);
-      print(movies.data?.movies?[i].smallCoverImage);
-    }
+
     // print(movies.data?.length);
+    return movies;
   } else {
-    print(response.statusCode);
+    return null;
   }
 }
