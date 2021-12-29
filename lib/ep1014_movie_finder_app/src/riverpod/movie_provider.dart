ac2aa00b944679fce06bfaa45058bc78ac67d65c
Dreamwalker <aristojeff@gmail.com>
2021-12-29 19:51:41 +0900

    Create movie_provider.dart

---
+++ lib/ep1014_movie_finder_app/src/riverpod/movie_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_notebook_17th/ep1014_movie_finder_app/src/api/movie_api.dart';
+import 'package:flutter_notebook_17th/ep1014_movie_finder_app/src/model/movies.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final movieProvider = FutureProvider<Movies>((ref){
+  return getMovies();
+});
