e194493c6dacb6d4a18a4e2d98e225ff1000c7e1
Dreamwalker <aristojeff@gmail.com>
2021-12-30 20:57:48 +0900

    Update real_estate_main_page.dart

--- lib/ep1018_real_estate_app/src/ui/real_estate_main_page.dart
+++ lib/ep1018_real_estate_app/src/ui/real_estate_main_page.dart
@@ -14,7 +14,26 @@ class _RealEstateMainPageState extends State<RealEstateMainPage> {
       backgroundColor: Colors.black,
       body: Column(
         children: [
-
+          Container(
+            height: 72,
+            decoration: BoxDecoration(color: Colors.blue),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 82,
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+          ),
+          Text("newly Added"),
+          Expanded(
+              child: Container(
+            color: Colors.pink,
+          )),
+          Container(
+            height: 72,
+            color: Colors.orange,
+          )
         ],
       ),
     );
