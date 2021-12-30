8e213fb260d09fd9b4bf45cc0ed5c3ac7326d941
Dreamwalker <aristojeff@gmail.com>
2021-12-30 21:00:07 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_17th/ep1018_real_estate_app/real_estate_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'ep1014_movie_finder_app/movie_finder_app.dart';
 
 void main() {
-  runApp(ProviderScope(child: MovieFinderApp()));
+  runApp(const ProviderScope(child: RealEstateApp()));
 }
