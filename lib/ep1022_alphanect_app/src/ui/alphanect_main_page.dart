53f8d85e8bd00988adf6957b4d056d9a0410a701
Dreamwalker <aristojeff@gmail.com>
2022-01-03 19:49:09 +0900

    Update alphanect_main_page.dart

--- lib/ep1022_alphanect_app/src/ui/alphanect_main_page.dart
+++ lib/ep1022_alphanect_app/src/ui/alphanect_main_page.dart
@@ -15,7 +15,12 @@ class AlphanectMainPage extends StatelessWidget {
                   onPressed: () {},
                   icon: Icon(Icons.menu),
                   color: Colors.grey,
-                )
+                ),
+                Icon(Icons.camera, color: Colors.blueAccent,),
+                Text("Whitelabel",style: TextStyle(
+                  color: Colors.blueAccent,
+                ),),
+                IconButton(onPressed: (){}, icon: Icon(Icons.))
               ],
             )
           ],
