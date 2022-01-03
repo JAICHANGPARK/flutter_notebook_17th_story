f5437c43c2f6888218d6bc2f6686789fa3032db0
Dreamwalker <aristojeff@gmail.com>
2022-01-03 19:42:44 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1018_real_estate_app/real_estate_app.dart';
+import 'package:flutter_notebook_17th/ep1022_alphanect_app/alphanect_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: RealEstateApp()));
+  runApp(const ProviderScope(child: AlphanectApp()));
 }
