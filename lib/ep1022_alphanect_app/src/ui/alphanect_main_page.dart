a1bdf556ac07eeabd029e49ab5b744f5b6ffd771
Dreamwalker <aristojeff@gmail.com>
2022-01-03 19:44:49 +0900

    Create alphanect_main_page.dart

---
+++ lib/ep1022_alphanect_app/src/ui/alphanect_main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class AlphanectMainPage extends StatelessWidget {
+  const AlphanectMainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
