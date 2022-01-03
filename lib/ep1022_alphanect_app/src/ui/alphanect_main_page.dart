f459b6695bd6a6d9a3d2fd86ec1fa67e27cf0e09
Dreamwalker <aristojeff@gmail.com>
2022-01-03 19:47:21 +0900

    Update alphanect_main_page.dart

--- lib/ep1022_alphanect_app/src/ui/alphanect_main_page.dart
+++ lib/ep1022_alphanect_app/src/ui/alphanect_main_page.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 
-
 class AlphanectMainPage extends StatelessWidget {
   const AlphanectMainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: SafeArea(
         child: Column(
           children: [
-            
-
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.menu),
+                  color: Colors.grey,
+                )
+              ],
+            )
           ],
         ),
       ),
-
     );
   }
 }
