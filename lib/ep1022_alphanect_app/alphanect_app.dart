3c8690b280cff9d8e457a814a01cce2b8c64108d
Dreamwalker <aristojeff@gmail.com>
2022-01-03 19:43:09 +0900

    Create alphanect_app.dart

---
+++ lib/ep1022_alphanect_app/alphanect_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AlphanectApp extends StatelessWidget {
+  const AlphanectApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
