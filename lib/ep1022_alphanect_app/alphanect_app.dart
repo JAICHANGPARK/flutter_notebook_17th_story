c7002fdb171bb1cb2d92f81bdd4c655bb15bde6c
Dreamwalker <aristojeff@gmail.com>
2022-01-03 19:44:44 +0900

    Update alphanect_app.dart

--- lib/ep1022_alphanect_app/alphanect_app.dart
+++ lib/ep1022_alphanect_app/alphanect_app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_17th/ep1022_alphanect_app/src/ui/alphanect_main_page.dart';
 
 class AlphanectApp extends StatelessWidget {
   const AlphanectApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: AlphanectMainPage(),
+    );
   }
 }
