5e7a01724368077e0cd816b6e9d3f1e58106cd12
Dreamwalker <aristojeff@gmail.com>
2022-01-06 20:49:25 +0900

    Create bulletin_news_profile_page.dart

---
+++ lib/ep1025_bulletin_news_app/src/ui/bulletin_news_profile_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class BulletinNewsProfilePage extends StatelessWidget {
+  const BulletinNewsProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
