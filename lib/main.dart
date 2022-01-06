537bba8f7ce606a3ca89e2ebf9eebadd449f2b55
Dreamwalker <aristojeff@gmail.com>
2022-01-06 20:49:18 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1022_alphanect_app/alphanect_app.dart';
+import 'package:flutter_notebook_17th/ep1025_bulletin_news_app/bullentin_news_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: AlphanectApp()));
+  runApp(const ProviderScope(child: BulletinNewsApp()));
 }
