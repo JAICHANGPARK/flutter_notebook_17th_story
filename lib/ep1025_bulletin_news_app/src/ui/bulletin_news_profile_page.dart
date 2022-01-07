5ea037dc3e1d06f5670472fdd242e6ae533653de
Dreamwalker <aristojeff@gmail.com>
2022-01-07 20:57:44 +0900

    Update bulletin_news_profile_page.dart

--- lib/ep1025_bulletin_news_app/src/ui/bulletin_news_profile_page.dart
+++ lib/ep1025_bulletin_news_app/src/ui/bulletin_news_profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_17th/ep1025_bulletin_news_app/src/provider/bulletin_news_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class BulletinNewsProfilePage extends StatelessWidget {
   const BulletinNewsProfilePage({Key? key}) : super(key: key);
@@ -58,104 +60,109 @@ class BulletinNewsProfilePage extends StatelessWidget {
               bottom: 0,
               right: 0,
               left: 0,
-              child: Container(
-                height: 84,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      width: 48,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Container(),
-                          ),
-                          IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
-                          const Spacer(),
-                          Container(
-                            height: 3,
-                            decoration: const BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(4),
-                                topRight: Radius.circular(4),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
+              child: Consumer(
+                builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                  final index = ref.watch(bulletinNewsTabProvider);
+                  return Container(
+                    height: 84,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
                     ),
-                    SizedBox(
-                      width: 48,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Container(),
-                          ),
-                          IconButton(onPressed: () {}, icon: const Icon(Icons.location_searching)),
-                          const Spacer(),
-                          Container(
-                            height: 3,
-                            decoration: const BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(4),
-                                topRight: Radius.circular(4),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: 48,
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Container(),
                               ),
-                            ),
-                          )
-                        ],
-                      ),
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
+                              const Spacer(),
+                              Container(
+                                height: 3,
+                                decoration: const BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(4),
+                                    topRight: Radius.circular(4),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 48,
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Container(),
+                              ),
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.location_searching)),
+                              const Spacer(),
+                              Container(
+                                height: 3,
+                                decoration: const BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(4),
+                                    topRight: Radius.circular(4),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 48,
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Container(),
+                              ),
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.all_inbox)),
+                              const Spacer(),
+                              Container(
+                                height: 3,
+                                decoration: const BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(4),
+                                    topRight: Radius.circular(4),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 48,
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Container(),
+                              ),
+                              IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
+                              const Spacer(),
+                              Container(
+                                height: 3,
+                                decoration: const BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(4),
+                                    topRight: Radius.circular(4),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                    SizedBox(
-                      width: 48,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Container(),
-                          ),
-                          IconButton(onPressed: () {}, icon: const Icon(Icons.all_inbox)),
-                          const Spacer(),
-                          Container(
-                            height: 3,
-                            decoration: const BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(4),
-                                topRight: Radius.circular(4),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      width: 48,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Container(),
-                          ),
-                          IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
-                          const Spacer(),
-                          Container(
-                            height: 3,
-                            decoration: const BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(4),
-                                topRight: Radius.circular(4),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                  );
+                },
               ),
             )
           ],
