fc56d324973854c7b92a8a87f3fb6431dfad6809
Dreamwalker <aristojeff@gmail.com>
2022-01-10 18:56:51 +0900

    Update workouts_app.dart

--- lib/ep1029_workouts_app/workouts_app.dart
+++ lib/ep1029_workouts_app/workouts_app.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class WorkoutsApp extends StatelessWidget {
+  const WorkoutsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WorkoutsMainPage(),
+    );
+  }
+}
