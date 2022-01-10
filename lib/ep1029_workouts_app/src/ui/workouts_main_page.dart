1c33dad15e80823584b4cbb8f344b2a59f259085
Dreamwalker <aristojeff@gmail.com>
2022-01-10 19:11:08 +0900

    Update workouts_main_page.dart

--- lib/ep1029_workouts_app/src/ui/workouts_main_page.dart
+++ lib/ep1029_workouts_app/src/ui/workouts_main_page.dart
@@ -11,93 +11,113 @@ class _WorkoutsMainPageState extends State<WorkoutsMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: const [],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 420,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                      ),
+                    ),
+                    SizedBox(height: 16,),
+                    Container(
+                      height: 360,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Container(
-            height: 72,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.feed_outlined),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text("Feed",style: TextStyle(
-                        fontSize: 12
-                      ),),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.fitness_center),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text("Workouts",style: TextStyle(
-                          fontSize: 12
-                      ),),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Center(
-                    child: CircleAvatar(
-                      radius: 24,
-                      child: Icon(Icons.add),
+            Container(
+              height: 72,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.feed_outlined),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "Feed",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.fastfood_outlined),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text("Meals",style: TextStyle(
-                          fontSize: 12
-                      ),),
-                    ],
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.fitness_center),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "Workouts",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.person),
-                      SizedBox(
-                        height: 8,
+                  Expanded(
+                    child: Center(
+                      child: CircleAvatar(
+                        radius: 24,
+                        child: Icon(Icons.add),
                       ),
-                      Text("Profile",style: TextStyle(
-                          fontSize: 12
-                      ),),
-                    ],
+                    ),
                   ),
-                )
-              ],
-            ),
-          )
-        ],
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.fastfood_outlined),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "Meals",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.person),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "Profile",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
