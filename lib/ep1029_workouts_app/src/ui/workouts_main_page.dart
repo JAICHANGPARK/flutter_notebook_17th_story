6c29b8f45f444497d70295f17485bc519b3e30a3
Dreamwalker <aristojeff@gmail.com>
2022-01-10 18:57:31 +0900

    Create workouts_main_page.dart

---
+++ lib/ep1029_workouts_app/src/ui/workouts_main_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class WorkoutsMainPage extends StatefulWidget {
+  const WorkoutsMainPage({Key? key}) : super(key: key);
+
+  @override
+  _WorkoutsMainPageState createState() => _WorkoutsMainPageState();
+}
+
+class _WorkoutsMainPageState extends State<WorkoutsMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
