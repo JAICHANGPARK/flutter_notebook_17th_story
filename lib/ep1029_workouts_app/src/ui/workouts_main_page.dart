12a64fd621156de16abc32a8d60e0cb714707d6c
Dreamwalker <aristojeff@gmail.com>
2022-01-10 19:03:05 +0900

    Update workouts_main_page.dart

--- lib/ep1029_workouts_app/src/ui/workouts_main_page.dart
+++ lib/ep1029_workouts_app/src/ui/workouts_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class WorkoutsMainPage extends StatefulWidget {
   const WorkoutsMainPage({Key? key}) : super(key: key);
 
@@ -12,12 +11,61 @@ class _WorkoutsMainPageState extends State<WorkoutsMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [],
+              ),
+            ),
+          ),
+          Container(
+            height: 64,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(Icons.feed_outlined),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text("Feed"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.feed_outlined),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text("Workouts"),
+                  ],
+                ),
+                FloatingActionButton(onPressed: () {}),
+                Column(
+                  children: [
+                    Icon(Icons.feed_outlined),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text("Meals"),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.feed_outlined),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text("Profile"),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
