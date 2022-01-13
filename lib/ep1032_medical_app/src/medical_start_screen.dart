ff5e2e09ac2fe01ba0e59b57aee8101d363fb621
Dreamwalker <aristojeff@gmail.com>
2022-01-13 20:30:51 +0900

    Create medical_start_screen.dart

---
+++ lib/ep1032_medical_app/src/medical_start_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MedicalStartScreen extends StatelessWidget {
+  const MedicalStartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
+
