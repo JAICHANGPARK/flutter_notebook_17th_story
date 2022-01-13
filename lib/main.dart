3ab94a408cb656cd487a17b9ef23a65f6261c01a
Dreamwalker <aristojeff@gmail.com>
2022-01-13 20:29:44 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1029_workouts_app/workouts_app.dart';
+import 'package:flutter_notebook_17th/ep1032_medical_app/medical_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: WorkoutsApp()));
+  runApp(const ProviderScope(child: MedicalApp()));
 }
