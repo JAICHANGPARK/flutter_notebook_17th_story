c87b60f887350d52f6eff4877463627e1481cfeb
Dreamwalker <aristojeff@gmail.com>
2022-01-13 20:33:46 +0900

    Update medical_app.dart

--- lib/ep1032_medical_app/medical_app.dart
+++ lib/ep1032_medical_app/medical_app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'src/medical_start_screen.dart';
+
 class MedicalApp extends StatelessWidget {
   const MedicalApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: MedicalStartScreen(),
+    );
   }
 }
