506ff955a0255dca6b6639fea30a211c1336578a
Dreamwalker <aristojeff@gmail.com>
2022-01-14 20:50:48 +0900

    Create medical_main_page.dart

---
+++ lib/ep1032_medical_app/src/medical_main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class MedicalMainPage extends StatelessWidget {
+  const MedicalMainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
