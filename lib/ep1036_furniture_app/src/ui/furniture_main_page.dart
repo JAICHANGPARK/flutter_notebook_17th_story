fda9fe54dd617d57d31ff8f5e107c6de59b50e24
Dreamwalker <aristojeff@gmail.com>
2022-01-17 20:09:47 +0900

    Update furniture_main_page.dart

--- lib/ep1036_furniture_app/src/ui/furniture_main_page.dart
+++ lib/ep1036_furniture_app/src/ui/furniture_main_page.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 class FurnitureMainPage extends ConsumerWidget {
   const FurnitureMainPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, ref) {
-    return Scaffold(
-
-    );
+  Widget build(context, ref) {
+    return Scaffold();
   }
 }
