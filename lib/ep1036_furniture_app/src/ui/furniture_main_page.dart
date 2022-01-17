145f1f1f2611f7fb316697808cc5c0b36be422b2
Dreamwalker <aristojeff@gmail.com>
2022-01-17 20:22:32 +0900

    Update furniture_main_page.dart

--- lib/ep1036_furniture_app/src/ui/furniture_main_page.dart
+++ lib/ep1036_furniture_app/src/ui/furniture_main_page.dart
@@ -9,17 +9,57 @@ class FurnitureMainPage extends ConsumerWidget {
   Widget build(context, ref) {
     final menuIndex = ref.watch(furnitureMenuProvider);
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Placeholder(),
-            flex: 20,
-          ),
-          Expanded(
-            child: Placeholder(),
-            flex: 2,
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Placeholder(),
+              flex: 20,
+            ),
+            Divider(
+              height: 0,
+              color: Colors.grey,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: IconButton(
+                        iconSize: 32,
+                    icon: Icon(Icons.home_outlined),
+                    color: menuIndex == FurnitureMenu.home ? Colors.indigoAccent : Colors.black,
+                    onPressed: () {},
+                  )),
+                  Expanded(
+                      child: IconButton(
+                        iconSize: 32,
+                        icon: Icon(Icons.favorite_border),
+                        color: menuIndex == FurnitureMenu.favorite ? Colors.indigoAccent : Colors.black,
+                        onPressed: () {},
+                      )),
+                  Expanded(
+                      child: IconButton(
+                        iconSize: 32,
+                        icon: Icon(Icons.shopping_bag_outlined),
+                        color: menuIndex == FurnitureMenu.favorite ? Colors.indigoAccent : Colors.black,
+                        onPressed: () {},
+                      )),
+                  Expanded(
+                      child: IconButton(
+                        iconSize: 32,
+                        icon: Icon(Icons.person_outlined),
+                        color: menuIndex == FurnitureMenu.favorite ? Colors.indigoAccent : Colors.black,
+                        onPressed: () {},
+                      )),
+                ],
+              ),
+              flex: 2,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
