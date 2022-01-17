f939af658278bde24c1be5372258c8ba96718990
Dreamwalker <aristojeff@gmail.com>
2022-01-17 20:22:27 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1032_medical_app/medical_app.dart';
+import 'package:flutter_notebook_17th/ep1036_furniture_app/funiture_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MedicalApp()));
+  runApp(const ProviderScope(child: FurnitureApp()));
 }
