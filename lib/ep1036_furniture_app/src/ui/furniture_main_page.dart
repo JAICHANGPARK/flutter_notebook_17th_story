3b5a22f8b3539423e1df44b3c57fead7502dbb2a
Dreamwalker <aristojeff@gmail.com>
2022-01-17 20:14:24 +0900

    Update furniture_main_page.dart

--- lib/ep1036_furniture_app/src/ui/furniture_main_page.dart
+++ lib/ep1036_furniture_app/src/ui/furniture_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_17th/ep1036_furniture_app/src/provider/furniture_menu_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FurnitureMainPage extends ConsumerWidget {
@@ -6,6 +7,20 @@ class FurnitureMainPage extends ConsumerWidget {
 
   @override
   Widget build(context, ref) {
-    return Scaffold();
+    final menuIndex = ref.watch(furnitureMenuProvider);
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Placeholder(),
+            flex: 20,
+          ),
+          Expanded(
+            child: Placeholder(),
+            flex: 2,
+          )
+        ],
+      ),
+    );
   }
 }
