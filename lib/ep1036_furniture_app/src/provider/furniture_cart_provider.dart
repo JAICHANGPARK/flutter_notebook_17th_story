e2f648aacd4a3d31cbff00ce4ba563297c8b5cba
Dreamwalker <aristojeff@gmail.com>
2022-01-19 19:54:31 +0900

    Update furniture_cart_provider.dart

--- lib/ep1036_furniture_app/src/provider/furniture_cart_provider.dart
+++ lib/ep1036_furniture_app/src/provider/furniture_cart_provider.dart
@@ -1,1 +1,21 @@
+import 'package:flutter_notebook_17th/ep1036_furniture_app/src/model/furniture_cart_item.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final furnitureCartProvider = StateProvider<List<CartItem>>((ref) => [
+  CartItem()
+
+]);
+
+class FurnitureCartItems extends StateNotifier{
+  FurnitureCartItems(state) : super(state);
+
+  List<CartItem> _items = [
+    CartItem(),
+    CartItem()
+  ];
+
+  incrementItem(int index){
+
+  }
+
+}
