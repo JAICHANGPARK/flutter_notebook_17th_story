0cb03d611f535c5ba5c9b86aaad39991c3454e3d
Dreamwalker <aristojeff@gmail.com>
2022-01-19 19:59:15 +0900

    Update furniture_cart_item.dart

--- lib/ep1036_furniture_app/src/model/furniture_cart_item.dart
+++ lib/ep1036_furniture_app/src/model/furniture_cart_item.dart
@@ -4,5 +4,11 @@ part 'furniture_cart_item.freezed.dart';
 
 @freezed
 class CartItem with _$CartItem {
-  factory CartItem({String? img, String? title, int? price, String? color}) = _CartItem;
+  factory CartItem({
+    String? img,
+    String? title,
+    int? price,
+    String? color,
+    String? subtitle,
+  }) = _CartItem;
 }
