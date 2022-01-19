b28de26836600a6d5144b4c32af9c9a94282b021
Dreamwalker <aristojeff@gmail.com>
2022-01-19 19:59:27 +0900

    Update furniture_cart_provider.dart

--- lib/ep1036_furniture_app/src/provider/furniture_cart_provider.dart
+++ lib/ep1036_furniture_app/src/provider/furniture_cart_provider.dart
@@ -2,26 +2,37 @@ import 'package:flutter_notebook_17th/ep1036_furniture_app/src/model/furniture_c
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final furnitureCartProvider = StateProvider<List<CartItem>>((ref) => [
-  CartItem()
+      CartItem(
+          title: "George",
+          color: "Black",
+          price: 299,
+          img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
+          subtitle: "Modern Couch"),
+      CartItem(
+          title: "George",
+          color: "Black",
+          price: 299,
+          img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
+          subtitle: "Modern Couch"),
+    ]);
 
-]);
-
-class FurnitureCartItems extends StateNotifier{
+class FurnitureCartItems extends StateNotifier {
   FurnitureCartItems(state) : super(state);
 
   List<CartItem> _items = [
     CartItem(
-      title: "George",
-      color: "Black",
-      price: 299,
-      img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
-
-    ),
-    CartItem()
+        title: "George",
+        color: "Black",
+        price: 299,
+        img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
+        subtitle: "Modern Couch"),
+    CartItem(
+        title: "George",
+        color: "Black",
+        price: 299,
+        img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
+        subtitle: "Modern Couch"),
   ];
 
-  incrementItem(int index){
-
-  }
-
+  incrementItem(int index) {}
 }
