d14702b3c2d7c1cacbe848f1be4278fba988dc99
Dreamwalker <aristojeff@gmail.com>
2022-01-20 20:02:56 +0900

    Update furniture_cart_provider.dart

--- lib/ep1036_furniture_app/src/provider/furniture_cart_provider.dart
+++ lib/ep1036_furniture_app/src/provider/furniture_cart_provider.dart
@@ -1,40 +1,40 @@
 import 'package:flutter_notebook_17th/ep1036_furniture_app/src/model/furniture_cart_item.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final furnitureCartProvider = StateProvider<List<CartItem>>((ref) => [
+// final furnitureCartProvider = StateProvider<List<CartItem>>((ref) => [
+//       CartItem(
+//           title: "George",
+//           color: "Black",
+//           price: 299,
+//           img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
+//           subtitle: "Modern Couch",
+//           count: 1),
+//       CartItem(
+//           title: "George",
+//           color: "Black",
+//           price: 299,
+//           img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
+//           subtitle: "Modern Couch",
+//           count: 1),
+//     ]);
+
+final furnitureCartProvider = StateNotifierProvider<FurnitureCartItems, List<CartItem>>((ref) => FurnitureCartItems([
       CartItem(
           title: "George",
           color: "Black",
           price: 299,
           img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
-          subtitle: "Modern Couch",
-          count: 1),
+          subtitle: "Modern Couch"),
       CartItem(
           title: "George",
           color: "Black",
           price: 299,
           img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
-          subtitle: "Modern Couch",
-          count: 1),
-    ]);
+          subtitle: "Modern Couch"),
+    ]));
 
-class FurnitureCartItems extends StateNotifier {
-  FurnitureCartItems(state) : super(state);
-
-  List<CartItem> _items = [
-    CartItem(
-        title: "George",
-        color: "Black",
-        price: 299,
-        img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
-        subtitle: "Modern Couch"),
-    CartItem(
-        title: "George",
-        color: "Black",
-        price: 299,
-        img: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376__340.jpg",
-        subtitle: "Modern Couch"),
-  ];
+class FurnitureCartItems extends StateNotifier<List<CartItem>> {
+  FurnitureCartItems(state) : super(state ?? []);
 
   incrementItem(int index) {}
 }
