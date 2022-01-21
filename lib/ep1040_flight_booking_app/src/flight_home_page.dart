6225cf848d8c222655f71e2a33ffced57f66a073
Dreamwalker <aristojeff@gmail.com>
2022-01-21 20:21:56 +0900

    Update flight_home_page.dart

--- lib/ep1040_flight_booking_app/src/flight_home_page.dart
+++ lib/ep1040_flight_booking_app/src/flight_home_page.dart
@@ -32,7 +32,21 @@ class _FlightHomePageState extends State<FlightHomePage> {
             Expanded(
               flex: 19,
               child: Column(
-                children: [],
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      height: 48,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(
+                            color: Colors.black,
+                          ),
+                          boxShadow: [BoxShadow(color: Colors.indigo[400]!, offset: Offset(4, 4))]),
+                    ),
+                  )
+                ],
               ),
             ),
             Expanded(
