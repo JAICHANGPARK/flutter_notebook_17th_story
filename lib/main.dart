c09ff9eeb7265090b1546c2e93b7998ba3b8466f
Dreamwalker <aristojeff@gmail.com>
2022-01-21 20:13:51 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,9 @@ import 'package:flutter_notebook_17th/ep1040_flight_booking_app/flight_booking_a
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: FlightBookingApp()));
+  runApp(
+    const ProviderScope(
+      child: FlightBookingApp(),
+    ),
+  );
 }
