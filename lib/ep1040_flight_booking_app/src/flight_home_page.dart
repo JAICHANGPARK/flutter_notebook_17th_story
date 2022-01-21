2530a43e92b3f179a250385e63dcbc01a942b7c6
Dreamwalker <aristojeff@gmail.com>
2022-01-21 20:12:08 +0900

    Update flight_home_page.dart

---
+++ lib/ep1040_flight_booking_app/src/flight_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightHomePage extends StatefulWidget {
+  const FlightHomePage({Key? key}) : super(key: key);
+
+  @override
+  _FlightHomePageState createState() => _FlightHomePageState();
+}
+
+class _FlightHomePageState extends State<FlightHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
