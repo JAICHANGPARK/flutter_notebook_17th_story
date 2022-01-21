ab2d5c37b0cbd12b8904d6f5908a2fc8e442fd8b
Dreamwalker <aristojeff@gmail.com>
2022-01-21 20:12:42 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1036_furniture_app/funiture_app.dart';
+import 'package:flutter_notebook_17th/ep1040_flight_booking_app/flight_booking_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: FurnitureApp()));
+  runApp(const ProviderScope(child: FlightBookingApp()));
 }
