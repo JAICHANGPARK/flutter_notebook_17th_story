807d70a5933ba5cc85fc5b43f46810b7f4853dee
Dreamwalker <aristojeff@gmail.com>
2022-01-24 20:08:13 +0900

    Update small_talk_home_page.dart

--- lib/ep1043_small_talk_app/src/ui/small_talk_home_page.dart
+++ lib/ep1043_small_talk_app/src/ui/small_talk_home_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-
 class SmallTalkHomePage extends StatelessWidget {
   const SmallTalkHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        centerTitle: true,
+        leading: CircleAvatar(),
+        title: Text("small talk"),
+      ),
       body: Column(),
     );
   }
