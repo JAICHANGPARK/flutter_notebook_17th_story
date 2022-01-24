da8546690198a5a43fe9d97b53c9ec61e11613d8
Dreamwalker <aristojeff@gmail.com>
2022-01-24 20:09:44 +0900

    Update small_talk_app.dart

--- lib/ep1043_small_talk_app/small_talk_app.dart
+++ lib/ep1043_small_talk_app/small_talk_app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_17th/ep1043_small_talk_app/src/ui/small_talk_home_page.dart';
 
 class SmallTalkApp extends StatelessWidget {
   const SmallTalkApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        "/": (context) => SmallTalkHomePage(),
+      },
+    );
   }
 }
