c1cdac75b04d88ce1cec319baf41a3a61b62112b
Dreamwalker <aristojeff@gmail.com>
2022-01-24 20:05:55 +0900

    Update small_talk_app.dart

---
+++ lib/ep1043_small_talk_app/small_talk_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SmallTalkApp extends StatelessWidget {
+  const SmallTalkApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
