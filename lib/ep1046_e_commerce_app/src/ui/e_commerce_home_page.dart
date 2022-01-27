6ef02e991f6f8795c073c73a26178884ef35205e
Dreamwalker <aristojeff@gmail.com>
2022-01-27 19:49:36 +0900

    Update e_commerce_home_page.dart

--- lib/ep1046_e_commerce_app/src/ui/e_commerce_home_page.dart
+++ lib/ep1046_e_commerce_app/src/ui/e_commerce_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class ECommerceHomePage extends StatelessWidget {
   const ECommerceHomePage({Key? key}) : super(key: key);
@@ -6,7 +7,14 @@ class ECommerceHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: SlidingUpPanel(
+          panel: Container(),
+          body: Column(
+            children: [],
+          ),
+        ),
+      ),
     );
   }
 }
