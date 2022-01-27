25b45dd0de421a0b6cf7995228de3654cf73313a
Dreamwalker <aristojeff@gmail.com>
2022-01-27 19:45:59 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1043_small_talk_app/small_talk_app.dart';
+import 'package:flutter_notebook_17th/ep1046_e_commerce_app/e_commerce_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     const ProviderScope(
-      child: SmallTalkApp(),
+      child: ECommerceApp(),
     ),
   );
 }
