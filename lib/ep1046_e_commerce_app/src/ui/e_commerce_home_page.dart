bceec69d5858b813eb77795eb16ac0cdc5b28b8d
Dreamwalker <aristojeff@gmail.com>
2022-01-27 19:56:52 +0900

    Update e_commerce_home_page.dart

--- lib/ep1046_e_commerce_app/src/ui/e_commerce_home_page.dart
+++ lib/ep1046_e_commerce_app/src/ui/e_commerce_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
@@ -9,14 +10,16 @@ class ECommerceHomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SlidingUpPanel(
-          isDraggable: false,
-          maxHeight: MediaQuery.of(context).size.height / 2.5,
-          minHeight: MediaQuery.of(context).size.height / 2.5,
-          panel: Container(),
-          body: Column(
-            children: [],
-          ),
-        ),
+            isDraggable: false,
+            maxHeight: MediaQuery.of(context).size.height / 2,
+            minHeight: MediaQuery.of(context).size.height / 2,
+            panel: Container(),
+            body: CachedNetworkImage(
+              imageUrl: 'https://cdn.pixabay.com/photo/2019/06/02/17/33/woman-4246954_960_720.jpg',
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              fit: BoxFit.cover,
+            )),
       ),
     );
   }
