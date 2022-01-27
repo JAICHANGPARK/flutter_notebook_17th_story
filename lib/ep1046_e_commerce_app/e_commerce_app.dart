270fa95593d455e17350955151f7f472687ee74a
Dreamwalker <aristojeff@gmail.com>
2022-01-27 19:45:19 +0900

    Update e_commerce_app.dart

---
+++ lib/ep1046_e_commerce_app/e_commerce_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class ECommerceApp extends StatelessWidget {
+  const ECommerceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ECommerceHomePage(),
+    );
+  }
+}
