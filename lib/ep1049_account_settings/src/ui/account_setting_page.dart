a95ca4238ea84cd2fdade2fd904ed262f96a196d
Dreamwalker <aristojeff@gmail.com>
2022-01-30 17:17:06 +0900

    Update account_setting_page.dart

--- lib/ep1049_account_settings/src/ui/account_setting_page.dart
+++ lib/ep1049_account_settings/src/ui/account_setting_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
-
 class AccountSettingPage extends StatelessWidget {
   const AccountSettingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Placeholder(),
+            ),
+            Divider(
+              height: 0,
+              thickness: 1.2,
+              color: Colors.grey,
+            ),
+            Container(
+              height: 72,
+              decoration: BoxDecoration(color: Colors.white),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
