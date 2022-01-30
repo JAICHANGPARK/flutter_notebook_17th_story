1e04177cecf0bb3eaddf32aa58245af84980f4d6
Dreamwalker <aristojeff@gmail.com>
2022-01-30 17:14:40 +0900

    Update account_settings.dart

---
+++ lib/ep1049_account_settings/account_settings.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class AccountSettings extends StatelessWidget {
+  const AccountSettings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AccountSettingPage(),
+    );
+  }
+}
