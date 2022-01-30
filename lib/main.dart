1fdb93e31cdd06723883e6ab9699cf5cfffd229e
Dreamwalker <aristojeff@gmail.com>
2022-01-30 17:15:39 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_17th/ep1046_e_commerce_app/e_commerce_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'ep1049_account_settings/account_settings.dart';
 
 void main() {
   runApp(
     const ProviderScope(
-      child: ECommerceApp(),
+      child: AccountSettings(),
     ),
   );
 }
