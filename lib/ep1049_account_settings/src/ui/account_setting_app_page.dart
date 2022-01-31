da6d96fd970e8348e716b3aaacfdfbd85d3aa1e8
Dreamwalker <aristojeff@gmail.com>
2022-01-31 17:44:49 +0900

    Update account_setting_app_page.dart

---
+++ lib/ep1049_account_settings/src/ui/account_setting_app_page.dart
@@ -0,0 +1,345 @@
+import 'package:flutter/material.dart';
+
+
+class AccountSettingAppPage extends StatelessWidget {
+  const AccountSettingAppPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Colors.grey[200],
+                          child: const Icon(Icons.chevron_left),
+                          foregroundColor: Colors.black,
+                        ),
+                        CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Colors.grey[200],
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const Text(
+                      "Apps",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    const Text("Connected apps and services"),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Social profiles",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                height: 42,
+                                width: 42,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 12,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    "Facebook",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Connected",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              const CircleAvatar(
+                                backgroundColor: Color.fromRGBO(238, 238, 255, 1),
+                                foregroundColor: Color.fromRGBO(34, 34, 225, 1),
+                                child: Icon(Icons.check),
+                              ),
+                            ],
+                          ),
+                          const Divider(
+                            height: 32,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                height: 42,
+                                width: 42,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 12,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    "Twitter",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Not Connected",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              const CircleAvatar(
+                                backgroundColor: Color.fromRGBO(34, 34, 225, 1),
+                                foregroundColor: Colors.white,
+                                child: Icon(Icons.insert_link),
+                              ),
+                            ],
+                          ),
+                          const Divider(
+                            height: 32,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                height: 42,
+                                width: 42,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 12,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    "Google",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Not Connected",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              const CircleAvatar(
+                                backgroundColor: Color.fromRGBO(34, 34, 225, 1),
+                                foregroundColor: Colors.white,
+                                child: Icon(Icons.insert_link),
+                              ),
+                            ],
+                          ),
+                          const Divider(
+                            height: 32,
+                          ),
+                          Row(
+                            children: const [
+                              CircleAvatar(
+                                radius: 8,
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.white,
+                                child: Icon(
+                                  Icons.add,
+                                  size: 12,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 12,
+                              ),
+                              Text(
+                                "Add More",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Apps",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                height: 42,
+                                width: 42,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 12,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    "Dropbox",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Connected",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              const CircleAvatar(
+                                backgroundColor: Color.fromRGBO(238, 238, 255, 1),
+                                foregroundColor: Color.fromRGBO(34, 34, 225, 1),
+                                child: Icon(Icons.check),
+                              ),
+                            ],
+                          ),
+                          const Divider(
+                            height: 32,
+                          ),
+                          Row(
+                            children: const [
+                              CircleAvatar(
+                                radius: 8,
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.white,
+                                child: Icon(
+                                  Icons.add,
+                                  size: 12,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 12,
+                              ),
+                              Text(
+                                "Add More",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Divider(
+              height: 0,
+              thickness: 1.2,
+              color: Colors.grey[200],
+            ),
+            Container(
+              height: 72,
+              decoration: const BoxDecoration(color: Colors.white),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.inbox)),
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.bookmarks)),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    backgroundColor: const Color.fromRGBO(34, 34, 225, 1),
+                    child: const Icon(Icons.edit),
+                  ),
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.send)),
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
